4a6432e6aa5f3826da0ef2a5805dc2b985f959bd
Md. Tariquzzaman <tariq@Mds-MacBook-Air.local>
2023-07-17 14:19:55 +0600

    spacy name detection added to regex_algorithm

---
+++ backend/regex_algorithm/ML_Entity_Detection/trainer.py
@@ -0,0 +1,43 @@
+import random
+import spacy
+import json
+from spacy.training.example import Example
+import os
+
+print(os.getcwd())
+
+# Assuming your JSON file is named 'training_data.json'
+json_file_path = 'ML_Entity_Detection/training_data.json'
+
+with open(json_file_path, 'r', encoding='utf-8') as f:
+    data = json.load(f)
+
+# Load the language model
+nlp = spacy.load('en_core_web_lg')
+
+# Add the entity recognizer to the pipeline if it's not already there
+if 'ner' not in nlp.pipe_names:
+    ner = nlp.create_pipe('ner')
+    nlp.add_pipe(ner, last=True)
+
+# Assuming your data is in the format you provided
+
+training_data = []
+for example in data:
+    text = example['text']
+    entities = [(int(ent['start']), int(ent['end']), ent['label']) for ent in example['entities']]
+    training_data.append((text, {'entities': entities}))
+
+# Train the entity recognizer
+n_iter = 100
+for _ in range(n_iter):
+    for example in training_data:
+        text, entities = example
+        doc = nlp.make_doc(text)
+        gold = Example.from_dict(doc, entities)
+        nlp.update([gold], drop=0.5)
+
+
+output_dir = 'ML_Entity_Detection/model'
+nlp.to_disk(output_dir)
+
