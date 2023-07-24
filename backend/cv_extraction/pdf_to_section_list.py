67869f6b1dd68382fa24698f969fcdc09fb8f09b
Md. Tariquzzaman <tariq@Mds-MacBook-Air.local>
2023-07-24 18:47:35 +0600

    updated readme and initialized cv module

---
+++ backend/cv_extraction/pdf_to_section_list.py
@@ -0,0 +1,11 @@
+from langchain.document_loaders import PDFMinerPDFasHTMLLoader
+
+def read_pdf(pdf_file):
+    # Using PDFminer, we are extracting the PDF as a HTML file
+    # This process retains the necessary formatting info of the original PDF
+    # Now we can use features like checking font size, style etc
+    loader = PDFMinerPDFasHTMLLoader(pdf_file)
+
+    # loader expects multiple PDF, since we are using 1 pdf, we want the first item of the list.
+    # The first item is stored at the 0th index
+    data = loader.load()[0]
